b3a8cc4f918e4ef9138428eeb5317674fa809650
Per Rovegard <per@rovegard.se>
2011-12-28 14:00:37 +0100

    Wrote unit tests for child-of functionality among sysinfo nodes.

--- test/test_sysinfo.py
+++ test/test_sysinfo.py
@@ -139,3 +139,115 @@ class TestSysInfoLvmLvs(unittest.TestCase):
         lv = self.lvs[0]
         self.assertEqual("backup", lv.vg_name)
     
+class TestPartition(unittest.TestCase):
+    def test_that_hd_without_number_is_disk(self):
+        p = Partition("3 0 1000 hda".split(" "))
+        self.assertTrue(p.is_disk())
+
+    def test_that_sd_without_number_is_disk(self):
+        p = Partition("8 0 1000 sda".split(" "))
+        self.assertTrue(p.is_disk())
+
+    def test_that_hd_with_number_is_not_disk(self):
+        p = Partition("3 1 1000 hda1".split(" "))
+        self.assertFalse(p.is_disk())
+
+    def test_that_partition_is_part_for_disk(self):
+        d = Partition("8 0 1000 sda".split(" "))
+        p = Partition("8 1 1000 sda1".split(" "))
+        self.assertTrue(p.is_partition_for(d))
+
+    def test_that_partition_is_not_part_for_disk(self):
+        d = Partition("8 0 1000 sda".split(" "))
+        p = Partition("8 17 1000 sdb1".split(" "))
+        self.assertFalse(p.is_partition_for(d))
+
+    def test_that_partition_is_child_of_root_if_disk(self):
+        r = Root()
+        d = Partition("8 0 1000 sda".split(" "))
+        self.assertTrue(d.is_child_of(r))
+
+    def test_that_partition_is_not_child_of_root_if_partition(self):
+        r = Root()
+        p = Partition("8 1 1000 sda1".split(" "))
+        self.assertFalse(p.is_child_of(r))
+
+    def test_that_partition_is_child_of_disk(self):
+        d = Partition("8 0 1000 sda".split(" "))
+        p = Partition("8 1 1000 sda1".split(" "))
+        self.assertTrue(p.is_child_of(d))
+
+class TestLvmPhysicalVolume(unittest.TestCase):
+    def test_that_pv_is_child_of_disk_if_name_matches(self):
+        d = Partition("8 0 1000 sda".split(" "))
+        pv = LvmPhysicalVolume("/dev/sda 1000".split(" "))
+        self.assertTrue(pv.is_child_of(d))
+
+    def test_that_pv_is_not_child_of_disk_if_name_doesnt_match(self):
+        d = Partition("8 0 1000 sda".split(" "))
+        pv = LvmPhysicalVolume("/dev/sdb 1000".split(" "))
+        self.assertFalse(pv.is_child_of(d))
+
+    def test_that_pv_is_child_of_partition(self):
+        p = Partition("8 1 1000 sda1".split(" "))
+        pv = LvmPhysicalVolume("/dev/sda1 1000".split(" "))
+        self.assertTrue(pv.is_child_of(p))
+
+    def test_that_pv_is_child_of_raid_array(self):
+        r = RaidArray(("md1 /dev/sda1".split(" "), 1000))
+        pv = LvmPhysicalVolume("/dev/md1 1000".split(" "))
+        self.assertTrue(pv.is_child_of(r))
+
+    def test_that_pv_is_not_child_of_root(self):
+        r = Root()
+        pv = LvmPhysicalVolume("/dev/sda 1000".split(" "))
+        self.assertFalse(pv.is_child_of(r))
+
+class LvmVolumeGroupTest(unittest.TestCase):
+    def test_that_vg_is_child_of_pv_if_pv_among_names(self):
+        pv = LvmPhysicalVolume("/dev/sda 1000".split(" "))
+        vg = LvmVolumeGroup(["test", "1000", ["/dev/sda"]])
+        self.assertTrue(vg.is_child_of(pv))
+
+    def test_that_vg_is_not_child_of_pv_if_pv_not_among_names(self):
+        pv = LvmPhysicalVolume("/dev/sda 1000".split(" "))
+        vg = LvmVolumeGroup(["test", "1000", ["/dev/sdb"]])
+        self.assertFalse(vg.is_child_of(pv))
+
+    def test_that_vg_is_not_child_of_root(self):
+        r = Root()
+        vg = LvmVolumeGroup(["test", "1000", ["/dev/sdb"]])
+        self.assertFalse(vg.is_child_of(r))
+
+class LvmLogicalVolumeTest(unittest.TestCase):
+    def test_that_lv_is_child_of_vg_with_correct_name(self):
+        vg = LvmVolumeGroup(["test", "1000", ["/dev/sdb"]])
+        lv = LvmLogicalVolume("small test 1000".split(" "))
+        self.assertTrue(lv.is_child_of(vg))
+
+    def test_that_lv_is_not_child_of_vg_with_wrong_name(self):
+        vg = LvmVolumeGroup(["blob", "1000", ["/dev/sdb"]])
+        lv = LvmLogicalVolume("small test 1000".split(" "))
+        self.assertFalse(lv.is_child_of(vg))
+
+    def test_that_lv_is_not_child_of_root(self):
+        r = Root()
+        lv = LvmLogicalVolume("small test 1000".split(" "))
+        self.assertFalse(lv.is_child_of(r))
+
+class RaidArrayTest(unittest.TestCase):
+    def test_that_array_is_child_of_disk_among_names(self):
+        d = Partition("8 0 1000 /dev/sda".split(" "))
+        r = RaidArray(("md1 /dev/sda".split(" "), 1000))
+        self.assertTrue(r.is_child_of(d))
+
+    def test_that_array_is_not_child_of_disk_not_among_names(self):
+        d = Partition("8 16 1000 /dev/sdb".split(" "))
+        r = RaidArray(("md1 /dev/sda".split(" "), 1000))
+        self.assertFalse(r.is_child_of(d))
+
+    def test_that_array_is_child_of_partition_among_names(self):
+        p = Partition("8 1 1000 /dev/sda1".split(" "))
+        r = RaidArray(("md1 /dev/sda1".split(" "), 1000))
+        self.assertTrue(r.is_child_of(p))
+
