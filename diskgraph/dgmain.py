242033a71f8b0a130111d0d42f215d86e891ea32
Per Rovegard <per@rovegard.se>
2011-12-28 14:20:08 +0100

    Updated license information to be less intrusive, and wrote some module-level comments.

--- diskgraph/dgmain.py
+++ diskgraph/dgmain.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2011, Per Rovegård <per@rovegard.se>
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the authors nor the names of its contributors
-#    may be used to endorse or promote products derived from this software
-#    without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+"""Main script for diskgraph, a utility that creates a graph of disks, partitions, etc.
+present on the current Linux server.
+
+This script must be run as root, since the LVM commands require that.
+
+Pydot (http://code.google.com/p/pydot) is used to create a PNG image of the graph.
+
+Created and tested by Per Rovegård on a server running Ubuntu 11.04 with Python 2.7.1.
+
+Distributed under the 3-Clause BSD license (http://opensource.org/licenses/BSD-3-Clause,
+and LICENSE file).
+"""
+
+__author__ = "Per Rovegård"
+__version__ = "1.0"
+__license__ = "BSD-3-Clause"
 
 import os, sys
 
